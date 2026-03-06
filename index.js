db43e4b081dd6012c961ad87ac5459922b5e9b3a
Rishi Pradhan <rpradhan301@gmail.com>
2026-03-06 18:32:30 +0530

    Update index.js - removed scroll feature 2

--- index.js
+++ index.js
@@ -51,20 +51,7 @@ faqQuestions.forEach(question => {
 
 
 
-// Scroll
-
-// const header = document.querySelector("header");
-
-// window.addEventListener("scroll", () => {
-
-//     if (window.scrollY > 60) {
-//         header.classList.add("scrolled");
-//     } else {
-//         header.classList.remove("scrolled");
-//     }
-
-// });
-
 question.setAttribute("aria-expanded", !isOpen);
+
 
 
